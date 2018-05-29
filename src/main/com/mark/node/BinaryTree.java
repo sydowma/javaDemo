7c0907a5fe3e018061ccd94d5fe5f674300cfa63
magaofei <magaofei@100tal.com>
2018-05-29 13:09:54 +0800

    完善二叉树

--- src/main/com/mark/node/BinaryTree.java
+++ src/main/com/mark/node/BinaryTree.java
@@ -1,5 +1,7 @@
 package node;
 
+import javax.management.loading.PrivateClassLoader;
+
 public class BinaryTree {
 
     private static Node root;
@@ -8,13 +10,33 @@ public class BinaryTree {
         root = new Node(data);
     }
 
-    public void add (Node parent, Node child, String orientation) {
+    private void add (Node parent, Node child, String orientation) {
         if (orientation.equals("left")) {
             parent.setLeft(child);
         } else if (orientation.equals("right")) {
             parent.setRight(child);
         }
     }
+
+    private Node addRecursive(Node current, int value) {
+        if (current == null) {
+            return new Node(value);
+        }
+
+        if ( value < current.value ) {
+            current.setLeft( addRecursive(current.getLeft(), value) );
+        } else if ( value > current.getKey() ) {
+            current.setRight( addRecursive(current.getRight() , value));
+        } else {
+            return current;
+        }
+    }
+
+    private void add2 (int value) {
+        root = addRecursive(root, value);
+    }
+
+
 
     /**
      * 前序遍历
@@ -28,7 +50,7 @@ public class BinaryTree {
         preOrder(node.getRight());
     }
 
-    public static void main(String[] args) {
+    private static void test1 () {
         Node n1 = new Node(1);
         Node n2 = new Node(4);
         Node n3 = new Node(2);
@@ -40,9 +62,30 @@ public class BinaryTree {
         tree.add(n2, n3, "left");
         tree.add(n2, n4, "right");
 
+        /**
+         * 3 / \ 1 4 / \ 2 5
+         */
+
         System.out.println(tree);
-        
+
         preOrder(tree.root);
+    }
+
+    private static BinaryTree test2() {
+        BinaryTree bt = new BinaryTree();
+
+        bt.add2(6);
+        bt.add2(4);
+        bt.add2(8);
+        bt.add2(3);
+        bt.add2(5);
+        bt.add2(7);
+        bt.add2(9);
+        return bt;
+    }
+
+    public static void main(String[] args) {
+ 
 
     }
 
