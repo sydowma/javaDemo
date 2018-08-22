b9898388e817c223e5a372e578f96b87a80ef1da
Mark <mamian521@gmail.com>
2018-08-22 23:50:50 +0800

    update

---
+++ src/main/com/mark/collection/SimpleHashMap.java
@@ -0,0 +1,65 @@
+package main.com.mark.collection;
+import java.util.AbstractMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.ListIterator;
+import java.util.Map;
+
+public class SimpleHashMap<K, V> extends AbstractMap<K, V> {
+    static final int SIZE = 997;
+
+    @SuppressWarnings("unchecked")
+    LinkedList<MapEntry<K, V>>[] buckets = new LinkedList[SIZE];
+
+    @Overrid
+    public V put(K key, V value) {
+        V oldValue = null;
+        int index = Math.abs(key.hashCode()) % SIZE;
+        if (buckets[index] == null) {
+            buckets[index] = new LinkedList<MapEntry<K, V>>();
+        }
+        LinkedList<MapEntry<K, V>> bucket = buckets[index];
+        MapEntry<K, V> pair = new MapEntry<K, V>(key, value);
+        boolean found = false;
+        ListIterator<MapEntry<K, V>> it = bucket.ListIterator();
+        while(it.hasNext()) {
+            MapEntry<K, V> iPair = it.next();
+            if (iPair.getKey().equals(key)) {
+                oldValue = iPair.getValue();
+                it.set(pair);
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            buckets[index].add(pair);
+        }
+
+        return oldValue;
+    }
+
+    public V get(Object key) {
+        int index = Math.abs(key.hashCode()) % SIZE;
+        if (buckets[index] == null) {
+            return null;
+        }
+    }
+
+    public Set<Map.Entry<K, V>> entrySet() {
+        Set<Map.Entry<K, V>> set = new HashSet<Map.Entry<K, V>>();
+        for(LinkedList<MapEntry<K, V>> bucket : buckets) {
+            if (bucket == null) {
+                continue;
+            }
+            for (MapEntry<K, V> mpair : bucket) {
+                set.add(mpair);
+            }
+        }
+        return set;
+    }
+
+    public static void main(String[] args) {
+        SimpleHashMap<String, String> m = new SimpleHashMap<String, String>();
+        
+    }
+}
