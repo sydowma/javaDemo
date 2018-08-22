f3f7448c7beb350d73e9655003598a1a1dad18b4
Mark <mamian521@gmail.com>
2018-08-22 23:52:25 +0800

    去掉不相干代码

--- src/main/com/mark/concurrency/MainThread.java
+++ src/main/com/mark/concurrency/MainThread.java
@@ -1,12 +1,8 @@
 package main.com.mark.concurrency;
 
-import java.util.Collection;
-import java.util.Collections;
-
 public class MainThread {
     public static void main(String[] args) {
         LiftOff launch = new LiftOff();
         launch.run();
-        Collections.binarySearch(list, key);
     }
 }
